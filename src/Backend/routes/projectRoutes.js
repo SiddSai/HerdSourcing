7cd8283366ca72b6e28b79a0c3d2254d8e44b8a5
Siddharth Sai <2004siddharthsai@gmail.com>
2025-11-17 18:30:52 -0800

    Simple backend bugs fixed

--- src/Backend/routes/projectRoutes.js
+++ src/Backend/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/projectController');
+const controller = require('../controllers/projectControllers');
 
 // GET all projects
 router.get('/', controller.getAllProjects);
